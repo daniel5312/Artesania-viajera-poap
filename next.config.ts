00dfb07ca64459e0d803cbaf796acdc2a4f9b93c
daniel5312 <dvh062487@gmail.com>
2026-01-01 02:45:29 -0500

    next.js config mejorado deply vercel

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  typescript: { ignoreBuildErrors: true }, // Ahorra RAM al no validar tipos en el build
+  eslint: { ignoreDuringBuilds: true },    // Evita procesos pesados de linting
+  experimental: {
+    parallelServerBuildTraces: false,      // Clave para evitar el error SIGKILL (OOM)
+    serverMinification: false              // Reduce la carga del CPU durante la compilación
+  },
+  // Necesario para compatibilidad de librerías Web3 con Next 15+
+  transpilePackages: ['@solana/kit', '@selfxyz/core', '@selfxyz/qrcode']
 };
 
 export default nextConfig;
